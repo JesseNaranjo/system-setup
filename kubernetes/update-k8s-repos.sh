34946de4a0c26e1bd3da3db513880da519b29082
Jesse Naranjo <JesseNaranjo@users.noreply.github.com>
2024-07-25 15:09:40 -0400

    Create update-k8s-repos.sh

---
+++ kubernetes/update-k8s-repos.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+K8S_VERSION=v1.30
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$K8S_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$K8S_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$K8S_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$K8S_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
