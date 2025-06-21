f8a3bee387f0d3b0df916cb95322b79a9b9c0b20
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-21 14:31:39 -0400

    Update setup-containers.sh

--- lxc/setup-containers.sh
+++ lxc/setup-containers.sh
@@ -30,19 +30,30 @@ if [[ ! -f /etc/lxc/default.conf.original ]] then
 fi
 
 
-# Add subuid and subgid entries
+# Ensure user is allowed to have veth interfaces
 
-SUB_ENTRY="${LIMITED_USER}:${ID_NO}:65535"
+VETH_ENTRY="$LIMITED_USER veth lxcbr0 10"
 
-if ! grep -q "${SUB_ENTRY}" "/etc/subuid"; then
-	echo "Adding \"${SUB_ENTRY}\" to /etc/subuid..."
-	echo "${SUB_ENTRY}" | sudo tee -a /etc/subuid > /dev/null
+if ! grep -q "$VETH_ENTRY" /etc/lxc/lxc-usernet; then
+	echo "Adding \"$VETH_ENTRY\" to /etc/lxc/lxc-usernet..."
+	echo "$VETH_ENTRY" | sudo tee -a /etc/lxc/lxc-usernet > /dev/null
 	echo ""
 fi
 
-if ! grep -q "${SUB_ENTRY}" "/etc/subgid"; then
-	echo "Adding \"${SUB_ENTRY}\" to /etc/subgid..."
-	echo "${SUB_ENTRY}" | sudo tee -a /etc/subgid > /dev/null
+
+# Add subuid and subgid entries
+
+SUB_ENTRY="$LIMITED_USER:$ID_NO:65535"
+
+if ! grep -q "$SUB_ENTRY" /etc/subuid; then
+	echo "Adding \"$SUB_ENTRY\" to /etc/subuid..."
+	echo "$SUB_ENTRY" | sudo tee -a /etc/subuid > /dev/null
+	echo ""
+fi
+
+if ! grep -q "$SUB_ENTRY" /etc/subgid; then
+	echo "Adding \"$SUB_ENTRY\" to /etc/subgid..."
+	echo "$SUB_ENTRY" | sudo tee -a /etc/subgid > /dev/null
 	echo ""
 fi
 
@@ -64,30 +75,38 @@ echo ""
 
 # Prepare ${LIMITED_USER}'s default LXC configuration
 
-LIMITED_USER_HOME=/home/${LIMITED_USER}
-LIMITED_USER_CONFIG_LXC=${LIMITED_USER_HOME}/.config/lxc
+LIMITED_USER_HOME="/home/$LIMITED_USER"
+LIMITED_USER_CONFIG_LXC="$LIMITED_USER_HOME/.config/lxc"
 
 echo "Creating user's default LXC config..."
-echo "${LIMITED_USER_CONFIG_LXC}/default.conf"
-sudo mkdir -p ${LIMITED_USER_CONFIG_LXC}
+echo "$LIMITED_USER_CONFIG_LXC/default.conf"
+sudo mkdir -p "$LIMITED_USER_CONFIG_LXC"
 
-sudo tee ${LIMITED_USER_CONFIG_LXC}/default.conf > /dev/null <<EOF
+sudo tee "$LIMITED_USER_CONFIG_LXC/default.conf" > /dev/null <<EOF
 # ID Map must match range found in /etc/subuid and /etc/subgid for "$LIMITED_USER"
 lxc.idmap = u 0 $ID_NO 65535
 lxc.idmap = g 0 $ID_NO 65535
 
-#lxc.apparmor.profile = unconfined
+# AppArmor Profile "unconfined" is necessary for networking to work (as of 2025-06-21)
+lxc.apparmor.profile = unconfined
+
+lxc.net.0.name = eth0
+lxc.net.0.type = veth
+lxc.net.0.link = lxcbr0
+lxc.net.0.flags = up
 EOF
 
-sudo chmod +x ${LIMITED_USER_HOME}
-if [[ -f ${LIMITED_USER_HOME}/.local ]] then
-	sudo chmod +x ${LIMITED_USER_HOME}/.local
-	if [[ -f ${LIMITED_USER_HOME}/.local/share ]] then
-		sudo chmod +x ${LIMITED_USER_HOME}/.local/share
-		if [[ -f ${LIMITED_USER_HOME}/.local/share/lxc ]] then
-			sudo chmod +x ${LIMITED_USER_HOME}/.local/share/lxc
+echo ""
+
+sudo chmod -v +x "$LIMITED_USER_HOME"
+if [[ -d "$LIMITED_USER_HOME/.local" ]]; then
+	sudo chmod -v +x "$LIMITED_USER_HOME/.local"
+	if [[ -d "$LIMITED_USER_HOME/.local/share" ]]; then
+		sudo chmod -v +x "$LIMITED_USER_HOME/.local/share"
+		if [[ -d "$LIMITED_USER_HOME/.local/share/lxc" ]]; then
+			sudo chmod -v +x "$LIMITED_USER_HOME/.local/share/lxc"
 		fi
 	fi
 fi
-sudo chown ${LIMITED_USER}:${LIMITED_USER} ${LIMITED_USER_HOME}/.config/lxc
-sudo chown ${LIMITED_USER}:${LIMITED_USER} ${LIMITED_USER_HOME}/.config/lxc/default.conf
+sudo chown -v ${LIMITED_USER}:${LIMITED_USER} "$LIMITED_USER_CONFIG_LXC"
+sudo chown -v ${LIMITED_USER}:${LIMITED_USER} "$LIMITED_USER_CONFIG_LXC/default.conf"
