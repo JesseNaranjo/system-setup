d95f262e08092d05e56d46163f2da08be3a4532d
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-21 14:34:34 -0400

    Update stop-container.sh

--- lxc/stop-container.sh
+++ lxc/stop-container.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 ## Input parameters
+## ================
 
+if [[ $# -eq 0 || -z ${1-} ]]; then
+	printf 'Usage:  %s <container name>\n' "${0##*/}" >&2
+	exit 64  # 64 - EX_USAGE (sysexits.h)
+fi
 CONTAINER_NAME=$1
 
 ## No touchy
+## =========
 
-echo "Stopping LXC ${CONTAINER_NAME}..."
-sudo lxc-stop --name ${CONTAINER_NAME}
+echo "Stopping LXC $CONTAINER_NAME..."
+lxc-stop --name ${CONTAINER_NAME}
 
 sleep 1s
 
-sudo lxc-ls --fancy
+lxc-ls --fancy
