14f3db4870acd470d04531f35f105e68eddc5210
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-21 14:33:34 -0400

    Update start-container.sh

--- lxc/start-container.sh
+++ lxc/start-container.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 ## Input parameters
+## ================
 
+if [[ $# -eq 0 || -z ${1-} ]]; then
+	printf 'Usage:  %s <container name>\n' "${0##*/}" >&2
+	exit 64  # 64 - EX_USAGE (sysexits.h)
+fi
 CONTAINER_NAME=$1
 
 ## No touchy
+## =========
 
 echo "Starting LXC ${CONTAINER_NAME}..."
-sudo lxc-start --name ${CONTAINER_NAME}
+lxc-unpriv-start --name ${CONTAINER_NAME}
 
-x=5; while  [ $x -gt 0 ]; do echo "Attaching in $(( x-- ))..."; sleep 1s; done
+x=3; while  [ $x -gt 0 ]; do echo "Attaching in $(( x-- ))..."; sleep 1s; done
 
-sudo lxc-attach --name ${CONTAINER_NAME}
+lxc-ls --fancy
+lxc-unpriv-attach --name ${CONTAINER_NAME}
