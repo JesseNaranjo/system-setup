9871b59d9973f0adbe885e0d5136ffeb42d29ace
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-22 12:05:52 -0400

    Update _download-ollama-scripts.sh

--- llm/_download-ollama-scripts.sh
+++ llm/_download-ollama-scripts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 curl --remote-name-all --remote-time\
-		https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/ollama-remote.sh\
-		https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/ollama-screen.sh
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/ollama-remote.sh\
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/ollama-screen.sh
 
-chmod +x\
-		ollama-remote.sh\
-		ollama-screen.sh
-  
+chmod -vv +x\
+	ollama-remote.sh\
+	ollama-screen.sh
+ 
