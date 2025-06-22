57e43da040a42d8215138a8c75c1d6ae15661a94
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-22 12:03:49 -0400

    Update _download-k8s-scripts.sh

--- kubernetes/_download-k8s-scripts.sh
+++ kubernetes/_download-k8s-scripts.sh
@@ -1,12 +1,13 @@
-curl --remote-name-all --remote-time\
-		https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/install-update-helm.sh\
-		https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/start-k8s.sh\
-		https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/stop-k8s.sh\
-		https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/update-k8s-repos.sh
+#!/bin/bash
 
-chmod +x\
-		install-update-helm.sh\
-		start-k8s.sh\
-		stop-k8s.sh\
-		update-k8s-repos.sh
-  
+curl --remote-name-all --remote-time\
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/install-update-helm.sh\
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/start-k8s.sh\
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/stop-k8s.sh\
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/kubernetes/update-k8s-repos.sh
+
+chmod -v +x\
+	install-update-helm.sh\
+	start-k8s.sh\
+	stop-k8s.sh\
+	update-k8s-repos.sh
