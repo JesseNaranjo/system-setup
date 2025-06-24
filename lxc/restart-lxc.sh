f162abae04743c3e92b56230147ce17acbcb4b21
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-24 10:57:01 -0400

    Create restart-lxc.sh

---
+++ lxc/restart-lxc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Input parameters
+## ================
+
+if [[ $# -eq 0 || -z ${1-} ]]; then
+	printf 'Usage:  %s <container name>\n' "${0##*/}" >&2
+	exit 64  # 64 - EX_USAGE (sysexits.h)
+fi
+CONTAINER_NAME=$1
+
+## No touchy
+## =========
+
+./stop-lxc.sh ${CONTAINER_NAME}
+sleep 1s
+echo ""
+./start-lxc.sh ${CONTAINER_NAME}
