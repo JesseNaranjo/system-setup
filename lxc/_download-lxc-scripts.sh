9f6900060df82e6d8851c78a4bede0c9f01037de
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-24 10:57:28 -0400

    Update _download-lxc-scripts.sh

--- lxc/_download-lxc-scripts.sh
+++ lxc/_download-lxc-scripts.sh
@@ -2,12 +2,14 @@
 
 curl --remote-name-all --remote-time\
 	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/lxc/create-priv-lxc.sh\
+	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/lxc/restart-lxc.sh\
 	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/lxc/setup-lxc.sh\
 	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/lxc/start-lxc.sh\
 	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/lxc/stop-lxc.sh
 
 chmod -vv +x\
 	create-priv-lxc.sh\
+	restart-lxc.sh\
 	setup-lxc.sh\
 	start-lxc.sh\
 	stop-lxc.sh
