8f1f70a1479240a4089b1274b0db271fbd6969ed
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-06-30 18:38:46 -0400

    Update _download-ollama-scripts.sh

--- llm/_download-ollama-scripts.sh
+++ llm/_download-ollama-scripts.sh
@@ -4,7 +4,7 @@ if [[ $updated -eq 0 || -z $updated ]]; then
 	TEMP_SCRIPT_FILE=/tmp/_download-ollama-scripts.sh
 
 	rm $TEMP_SCRIPT_FILE
-	curl -o $TEMP_SCRIPT_FILE https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/_download-ollama-scripts.sh
+	curl --header 'Cache-Control: no-cache' --output $TEMP_SCRIPT_FILE https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/_download-ollama-scripts.sh
 
 	RED='\033[1;31m'
 	NC='\033[0m'
@@ -17,12 +17,14 @@ if [[ $updated -eq 0 || -z $updated ]]; then
 		chmod +x $TEMP_SCRIPT_FILE
 		$(export updated=1; $TEMP_SCRIPT_FILE)
 		mv $TEMP_SCRIPT_FILE ${BASH_SOURCE[0]}
+	else
+		rm $TEMP_SCRIPT_FILE
 	fi
 
 	exit 0
 fi
 
-curl --remote-name-all --remote-time\
+curl --header 'Cache-Control: no-cache' --remote-name-all --remote-time\
 	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/ollama-remote.sh\
 	https://raw.githubusercontent.com/JesseNaranjo/system-setup/refs/heads/main/llm/ollama-screen.sh
 
