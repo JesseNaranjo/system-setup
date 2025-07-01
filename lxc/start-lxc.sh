4b11ad6c6f4417d013edd421c807c8589f911977
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-07-01 13:19:16 -0400

    Update start-lxc.sh

--- lxc/start-lxc.sh
+++ lxc/start-lxc.sh
@@ -21,5 +21,10 @@ x=3; while  [ $x -gt 0 ]; do echo "Attaching in $(( x-- ))..."; sleep 1s; done
 echo ""
 lxc-ls --fancy
 
+# lxc-unpriv-attach reuses the calling environment in the container
+# - all env variables are passed through and, so, by default the container think
+# - that it's running as the user that attached into the lxc.
+# - And even though inside the container you may be root,
+# - the env variables are not setup correctly (for example, check $HOME without the --set-var argument)
 echo ""
 lxc-unpriv-attach --name ${CONTAINER_NAME} --set-var HOME=/root
