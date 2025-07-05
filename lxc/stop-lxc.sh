730f920df2aff1ccac918483cdabaaa11558bb52
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-07-05 08:20:33 -0400

    Update stop-lxc.sh to accept multiple lxc names as parameters

--- lxc/stop-lxc.sh
+++ lxc/stop-lxc.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+
 ## Input parameters
 ## ================
 
 if [[ $# -eq 0 || -z ${1-} ]]; then
-	printf 'Usage:  %s <container name>\n' "${0##*/}" >&2
+	printf 'Usage:  %s <container name> [[container name], ...]\n' "${0##*/}" >&2
 	exit 64  # 64 - EX_USAGE (sysexits.h)
 fi
-CONTAINER_NAME=$1
+
 
 ## No touchy
 ## =========
 
-echo "Stopping LXC $CONTAINER_NAME..."
-lxc-stop --name ${CONTAINER_NAME}
-
-sleep 1s
+for lxcName in "$@"; do
+	echo; echo "Stopping LXC ${lxcName}..."
+	lxc-stop --name "${lxcName}"
+	sleep 1s
+done
 
 echo
 lxc-ls --fancy
