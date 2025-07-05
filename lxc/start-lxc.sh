7347150aee8a4c01c50844d9dfa2b77171c339fd
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-07-05 08:20:07 -0400

    Update start-lxc.sh to accept multiple lxc names as parameters

--- lxc/start-lxc.sh
+++ lxc/start-lxc.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
+
 ## Input parameters
 ## ================
 
 if [[ $# -eq 0 || -z ${1-} ]]; then
-	printf 'Usage:  %s <container name>\n' "${0##*/}" >&2
+	printf 'Usage:  %s <container name> [[container name], ...]\n' "${0##*/}" >&2
 	exit 64  # 64 - EX_USAGE (sysexits.h)
 fi
-CONTAINER_NAME=$1
+
 
 ## No touchy
 ## =========
 
-echo "Starting LXC $CONTAINER_NAME..."
-lxc-unpriv-start --name ${CONTAINER_NAME}
+for lxcName in "$@"; do
+	echo; echo "Starting LXC ${lxcName}..."
+	lxc-unpriv-start --name "${lxcName}"
+	sleep 1s
+done
 
-echo
-x=3; while  [ $x -gt 0 ]; do echo "Attaching in $(( x-- ))..."; sleep 1s; done
+if [[ $# -eq 1 ]]; then
+	lxcName=$1
 
-echo
-lxc-ls --fancy
+	echo
+	x=3; while  [ $x -gt 0 ]; do echo "Attaching in $(( x-- ))..."; sleep 1s; done
 
-# lxc-unpriv-attach reuses the calling environment in the container
-# - all env variables are passed through and, so, by default the container think
-# - that it's running as the user that attached into the lxc.
-# - And even though inside the container you may be root,
-# - the env variables are not setup correctly (for example, check $HOME without the --set-var argument)
-echo
-lxc-unpriv-attach --name ${CONTAINER_NAME} --set-var HOME=/root -- /bin/bash -l
+	echo
+	lxc-ls --fancy
+
+	# lxc-unpriv-attach reuses the calling environment in the container
+	# - all env variables are passed through and, so, by default the container think
+	# - that it's running as the user that attached into the lxc.
+	# - And even though inside the container you may be root,
+	# - the env variables are not setup correctly (for example, check $HOME without the --set-var argument)
+	echo
+	lxc-unpriv-attach --name "${lxcName}" --set-var HOME=/root -- /bin/bash -l
+else
+	echo
+	lxc-ls --fancy
+fi
