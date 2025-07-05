4841743fb60275cba77a49602945531c7c9c2f2d
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-07-05 08:19:01 -0400

    Update restart-lxc.sh

--- lxc/restart-lxc.sh
+++ lxc/restart-lxc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+
 ## Input parameters
 ## ================
 
 if [[ $# -eq 0 || -z ${1-} ]]; then
-	printf 'Usage:  %s <container name>\n' "${0##*/}" >&2
+	printf 'Usage:  %s <container name> [[container name], ...]\n' "${0##*/}" >&2
 	exit 64  # 64 - EX_USAGE (sysexits.h)
 fi
-CONTAINER_NAME=$1
+
 
 ## No touchy
 ## =========
 
-./stop-lxc.sh ${CONTAINER_NAME}
+./stop-lxc.sh $@
 sleep 1s
-echo
-./start-lxc.sh ${CONTAINER_NAME}
+./start-lxc.sh $@
