746f29143aea9a56440f40490ecda6586683428f
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-08-17 12:54:17 -0400

    Update stop-lxc.sh to fix LXC names being passed as params

--- lxc/stop-lxc.sh
+++ lxc/stop-lxc.sh
@@ -2,12 +2,15 @@
 
 set -euo pipefail
 
+## No touchy
+## =========
+
 if [[ $# -eq 0 || -z ${1-} ]]; then
 	# No LXCs specified, so stop all running LXCs
 	lxc-ls --running
 	RUNNING=( $(/usr/bin/lxc-ls --running) )
 else
-	RUNNING=$@
+	RUNNING=("$@")
 fi
 
 for lxcName in "${RUNNING[@]}"; do
