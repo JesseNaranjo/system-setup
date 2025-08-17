e87d96a820f3e59cd14479637d067d40fb0d2ef7
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-08-17 12:56:43 -0400

    Update restart-lxc.sh

--- lxc/restart-lxc.sh
+++ lxc/restart-lxc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-
-## Input parameters
-## ================
-
-if [[ $# -eq 0 || -z ${1-} ]]; then
-	printf 'Usage:  %s <container name> [[container name], ...]\n' "${0##*/}" >&2
-	exit 64  # 64 - EX_USAGE (sysexits.h)
-fi
-
+set -euo pipefail
 
 ## No touchy
 ## =========
 
-./stop-lxc.sh $@
+if [[ $# -eq 0 || -z ${1-} ]]; then
+	# No LXCs specified, so stop all running LXCs
+	lxc-ls --running
+	RUNNING=( $(/usr/bin/lxc-ls --running) )
+else
+	RUNNING=("$@")
+fi
+
+./stop-lxc.sh "${RUNNING[@]}"
 sleep 1s
-./start-lxc.sh $@
+./start-lxc.sh "${RUNNING[@]}"
