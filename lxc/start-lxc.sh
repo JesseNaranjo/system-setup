03907844dd91c24dea10b570c942021424cef45c
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2025-08-17 12:54:31 -0400

    Update start-lxc.sh

--- lxc/start-lxc.sh
+++ lxc/start-lxc.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+## No touchy
+## =========
+
 if [[ $# -eq 0 || -z ${1-} ]]; then
 	printf 'Usage:  %s <container name> [[container name], ...]\n' "${0##*/}" >&2
 	exit 64  # 64 - EX_USAGE (sysexits.h)
