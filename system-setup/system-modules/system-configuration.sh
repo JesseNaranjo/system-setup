f8e9731c79710d8e09d01106018d85d79ed9f342
Jesse Naranjo <13751049+JesseNaranjo@users.noreply.github.com>
2026-08-19 14:48:23 -0400

    feat(tmux): improve pane border format and update session name display in status bar

--- system-setup/system-modules/system-configuration.sh
+++ system-setup/system-modules/system-configuration.sh
@@ -141,10 +141,10 @@ configure_tmux() {
     add_config_if_needed "tmux" "$config_file" "set -g window-active-style" '"bg=color233"' "active window background"
     add_config_if_needed "tmux" "$config_file" "set -g pane-border-lines" "double" "double-line pane borders"
     add_config_if_needed "tmux" "$config_file" "set -g pane-border-status" "top" "pane border status location"
-    add_config_if_needed "tmux" "$config_file" "set -g pane-border-format" '"#{?pane_active,#[fg=#ff0051]#[bg=#4aa244] ●#[fg=#000000],#[fg=#333333]#[bg=#999999] ○} #{pane_title} // #{pane_current_command} "' "pane border format"
-    add_config_if_needed "tmux" "$config_file" "set -g status-left" '"Session: #{session_name} // "' "session name in status bar"
+    add_config_if_needed "tmux" "$config_file" "set -g pane-border-format" '"#{?pane_active,#[fg=#ff0051]#[bg=#4aa244]  ●#[fg=#000000],#[fg=#333333]#[bg=#999999]  ○} #{pane_title}  //  #{pane_current_command} "' "pane border format"
+    add_config_if_needed "tmux" "$config_file" "set -g status-left" '""' "session name in status bar"
     add_config_if_needed "tmux" "$config_file" "set -g status-left-length" "0" "status-left max length"
-    add_config_if_needed "tmux" "$config_file" "set -g status-right" '"%a %b %d %I:%M"' "current date and time in status bar"
+    add_config_if_needed "tmux" "$config_file" "set -g status-right" '"Session: #{session_name} // %a %b %d %I:%M"' "current date and time in status bar"
     add_config_if_needed "tmux" "$config_file" "set -g status-right-length" "0" "status-right max length"
 
     # Remap prefix to Ctrl-a
